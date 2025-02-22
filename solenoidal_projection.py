f08bb529162038776d41e85ce9d6ff4a828cb54c
Vladimir Makarić <vladamakaric@gmail.com>
2025-02-22 15:18:52 +0100

    Heavily comment solenoidal_projection.

--- solenoidal_projection.py
+++ solenoidal_projection.py
@@ -13,7 +13,7 @@ class SolenoidalProjection:
     This is applied at the end of each simulation step to make sure the same
     amount of fluid is flowing in and out of every point.
 
-    Implementation:
+    Continuous math:
 
     Every vector field w is the sum of a solenoidal part u, and the
     gradient of a scalar field p: w = u + grad(p); aka Helmholtz Decomposition.
@@ -24,11 +24,49 @@ class SolenoidalProjection:
 
     We set the boundary conditions of p such that the resulting solenoidal 
     field (w - grapd(p)) flows around the boundary, ie. there is no flow in
-    the direction normal to the boundary:
+    the direction normal to the boundary (vector n):
 
-    normal*(w-grad(p)) = 0    =>    grad(p) = normal*w
+    n*(w-grad(p)) = 0    =>    n*grad(p) = n*w
 
-    This is a so-called Neumann boundary condition.
+    This is called a Neumann boundary condition, because we specify the
+    derivaitve of p at the boundary, not the value.
+
+    Discretization:
+
+    Our velocity vector field is a discrete grid, so we need to discretize
+    the continuous Poisson differential equation. This is done by using
+    finite difference forms of the divergence, laplacian, and the gradient.
+    For example, the derivative of field p wrt x at grid point [j,i] is:
+    (P[j,i+1] - P[j,i-1])/2.
+    
+    This gives us a linear equation for each fluid cell of the grid. We solve
+    this system of linear equations every frame using the pyamg library.
+
+    Boundary cells, where boundary conditions apply, are not explicitly solved
+    for, they are not unknowns in the system. They are a function of the
+    neighboring fluid cells according to the boundary condition. In the
+    literature these are sometimes called "ghost cells".
+
+    The boundary condition at each boundary cell (n*grad(p) = n*w), is
+    discretized using single differences:
+
+    n*[(bc-fn_x)*sign(n_x), (bc-fn_y)*sign(n_y)] = n*w
+
+    The single differences between the value of the boundary cell (bc) and
+    neighboring fluid cells along x and y (fn_x, fn_y), are multiplied by
+    the sign of the normal components (n_x, n_y) because they determine the
+    direction of the difference. For example, if the normal points to the left,
+    it means the fluid neighbor is to the right, and the single-diff derivaitve
+    is fn_x - bc.
+
+    We can rearange this equation to get the value of bc expressed in terms of
+    neighboring fluid cells:
+
+    bc = (fn_x*|n_x| + fn_y*|n_y| + n*w)/|n_x + n_y|
+
+    When the laplacian or divergence finite difference operators are applied on
+    the boundary, and the value of bc is needed, this function is substituted,
+    so that the system of equation is eniterly in terms of fluid cells.
     """
 
     def __init__(self, cells):
@@ -42,31 +80,11 @@ class SolenoidalProjection:
         self.boundary_normals = np.zeros(cells.shape + (2,))
         for c in cells.flat:
             match c:
-                case cell.BoundaryCell(index=index, normal=normal):
+                case cell.BoundaryCell(index=index, normal=normal): 
+                    # To see why we divide by |n_x + n_y|, see equation for the value
+                    # of a boundary cell (bc) in the class comment.
                     self.boundary_normals[index] = normal / np.sum(np.abs(normal))
 
-    def project(self, w: NDArray[np.float_]):
-        b = self._projection_b(w)
-        # Can extract residuals from this solve method for debugging.
-        x = self.multigrid_solver.solve(b)
-        P = np.zeros(shape=self.cells.shape)
-        P[self.indices.fluid] = x
-        for c in self.cells.flat:
-            match c:
-                case cell.BoundaryCell(index=index, normal=normal, x_diff=xd, y_diff=yd):
-                    v = w[index]
-                    # g = (other_x*abs(nx) + other_y*abs(ny) + b)/(np.abs(nx + ny))
-                    an_x = np.abs(normal[0])
-                    an_y = np.abs(normal[1])
-                    P[index] = np.dot(v, normal) / (an_x + an_y)
-                    if xd:
-                        P[index] += P[xd.fluid_cell.index] * an_x / (an_x + an_y)
-                    if yd:
-                        P[index] += P[yd.fluid_cell.index] * an_y / (an_x + an_y)
-        bs = self.boundary_gradient_stencil
-        w[:, :, 0] -= (P[bs.j, bs.i_right] - P[bs.j, bs.i_left]) / bs.id
-        w[:, :, 1] -= (P[bs.j_up, bs.i] - P[bs.j_down, bs.i]) / bs.jd
-        w[self.indices.obstacle] = [0, 0]
 
     def _coefficient_matrix(fluid_cells):
         A = np.zeros(shape=(len(fluid_cells), len(fluid_cells)))
@@ -79,11 +97,15 @@ class SolenoidalProjection:
                     case cell.FluidCell(num=num):
                         row[num] = 1
                     case cell.BoundaryCell(normal=normal, x_diff=x_diff, y_diff=y_diff):
+                        # Substitute the fluid cell coefficients from the boundary cell
+                        # expression (see class comment):
+                        #
+                        # bc = (fn_x*|n_x| + fn_y*|n_y| + n*w)/|n_x + n_y|
+                        #
+                        # The n*w/|n_x + n_y| goes on the RHS of the equation in
+                        # _projection_b.
                         an_x = np.abs(normal[0])
                         an_y = np.abs(normal[1])
-                        # Value of this ghost cell (g) obeys the following boundary condition:
-                        # (g-other_x)*abs(nx) + (g-other_y)*abs(ny) = n*w
-                        # g = (other_x*abs(nx) + other_y*abs(ny) + n*w)/(np.abs(nx + ny))
                         if x_diff:
                             row[x_diff.fluid_cell.num] += an_x / (an_x + an_y)
                         if y_diff:
@@ -92,15 +114,45 @@ class SolenoidalProjection:
                         raise ValueError("Neighbor must be a fluid or boundary cell")
         return A
 
+    def project(self, w: NDArray[np.float_]):
+        b = self._projection_b(w)
+        # Can extract residuals from this solve method for debugging.
+        x = self.multigrid_solver.solve(b)
+        P = np.zeros(shape=self.cells.shape)
+        P[self.indices.fluid] = x
+        for c in self.cells.flat:
+            match c:
+                case cell.BoundaryCell(index=index, normal=normal, x_diff=xd, y_diff=yd):
+                    v = w[index]
+                    # Value of the boundary cell is a funciton of neighboring fluid cells:
+                    # bc = (fn_x*|n_x| + fn_y*|n_y| + n*w)/|n_x + n_y|
+                    # (see class comment for details)
+                    an_x = np.abs(normal[0])
+                    an_y = np.abs(normal[1])
+                    P[index] = np.dot(v, normal) / (an_x + an_y)
+                    if xd:
+                        P[index] += P[xd.fluid_cell.index] * an_x / (an_x + an_y)
+                    if yd:
+                        P[index] += P[yd.fluid_cell.index] * an_y / (an_x + an_y)
+        bs = self.boundary_gradient_stencil
+        w[:, :, 0] -= (P[bs.j, bs.i_right] - P[bs.j, bs.i_left]) / bs.id
+        w[:, :, 1] -= (P[bs.j_up, bs.i] - P[bs.j_down, bs.i]) / bs.jd
+        w[self.indices.obstacle] = [0, 0]
+
+
     def _projection_b(self, w):
         s = cell.central_difference_stencil(self.cells.shape)
-        wdotn = np.sum(w * self.boundary_normals, axis=-1)
         divergence = (
             w[s.j, s.i_right, 0]
             - w[s.j, s.i_left, 0]
             + w[s.j_up, s.i, 1]
             - w[s.j_down, s.i, 1]
         ) / 2
+        # This is calculating n*w/|n_x + n_y|, which is part of the boundary
+        # equation that goes on the right hand side (fn terms go in the
+        # coefficient matrix in _coefficient_matrix):
+        # bc = (fn_x*|n_x| + fn_y*|n_y| + n*w)/|n_x + n_y|
+        wdotn = np.sum(w * self.boundary_normals, axis=-1)
         boundary_condition_rhs = (
             wdotn[s.j, s.i_right]
             + wdotn[s.j, s.i_left]
